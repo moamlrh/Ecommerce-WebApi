2e303d314021d9b630e0e246b5613008a344d2d9
Moaml RH <moaml.riyadh@gmail.com>
2023-06-30 18:27:07 +0300

    added role configuration

---
+++ Ecommerce.Repository/Configuration/RoleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Repository.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData(
+            new List<IdentityRole>
+            {
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Name = "User", NormalizedName = "USER" },
+            }
+        );
+    }
+}
