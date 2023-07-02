a5567149948ebc5e4cbf9b0531aa7d2a670c9fdb
Moaml RH <moaml.riyadh@gmail.com>
2023-07-03 00:31:26 +0300

    added validation attributes to Product table

--- Ecommerce.Entities/Models/Product.cs
+++ Ecommerce.Entities/Models/Product.cs
@@ -1,12 +1,22 @@
-﻿using Ecommerce.Shared;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Ecommerce.Shared;
 
 namespace Ecommerce.Entities.Models;
 
 public class Product
 {
     public Guid Id { get; set; }
+    [Required]
+    [MinLength(1)]
+    [MaxLength(255)]
     public string Name { get; set; }
+    [Column(TypeName = "decimal(15,2)")]
+    [Range(0, int.MaxValue)]
     public decimal Price { get; set; }
+    [Required]
+    [MinLength(5)]
     public string Description { get; set; }
     public string UserId { get; set; }
     public User User { get; set; }
