9edbdc304c96db06fb0450e1f19780eb7580bf61
moamlrh <moamll288@gmail.com>
2023-07-04 15:22:19 +0300

    removed quantity from product

--- Ecommerce.Entities/Models/Product.cs
+++ Ecommerce.Entities/Models/Product.cs
@@ -20,7 +20,6 @@ public class Product
     [Required]
     [MinLength(5)]
     public string Description { get; set; }
-    public int Quantity { get; set; }
     public string UserId { get; set; }
     public User User { get; set; }
     public ICollection<CartItem> CartItems { get; set; }
