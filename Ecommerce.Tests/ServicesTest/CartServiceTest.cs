62a4d8913f6206c32496341f4df0874d078f552d
moamlrh <moamll288@gmail.com>
2023-07-08 01:26:43 +0300

    added unit testing for remove product from cart with changes for whole unit class by adding the SUT

--- Ecommerce.Tests/ServicesTest/CartServiceTest.cs
+++ Ecommerce.Tests/ServicesTest/CartServiceTest.cs
@@ -4,10 +4,14 @@ using Ecommerce.Service;
 using Ecommerce.Service.Contracts;
 using Ecommerce.Contracts;
 using AutoMapper;
+using Ecommerce.Shared;
 
 public class CartServiceTests
 {
     private readonly ICartService _cartService;
+    private readonly User user;
+    private readonly Cart cart;
+    private readonly Product product;
     private readonly Mock<IMapper> _mockMapper;
     private readonly Mock<IRepositoryManager> _mockRepoManager;
     public CartServiceTests()
@@ -16,29 +20,26 @@ public class CartServiceTests
         _mockMapper = new Mock<IMapper>();
         _cartService = new CartService(_mockRepoManager.Object, _mockMapper.Object);
 
+        user = new User { Id = "1" };
+        product = new Product { Id = Guid.NewGuid(), Price = 39m, Name = "Test", UserId = user.Id };
+        cart = new Cart { Id = Guid.NewGuid(), UserId = user.Id, TotalPrice = product.Price };
+        cart.CartItems = new List<CartItem>
+        {
+            new CartItem { Id = cart.Id, Product= product, Cart = cart, ProductId = product.Id, Quantity = 1 }
+        };
     }
 
     [Fact]
     public async void CreateCartReturnCartAsResult()
     {
         // arragne
-        var TotalPrice = 10m;
-        var cartId = Guid.NewGuid();
-        var UserId = Guid.NewGuid();
-        var cart = new Cart
-        {
-            Id = cartId,
-            UserId = UserId.ToString(),
-            TotalPrice = TotalPrice,
-        };
-
         _mockRepoManager.Setup(x => x.CartRepository.CreateCart(cart)).Returns(Task.FromResult(cart));
 
         // act
-        var result = await _cartService.CreateCartAsync(UserId.ToString());
+        var result = await _cartService.CreateCartAsync(user.Id.ToString());
 
         // assert
-        Assert.Equal(UserId.ToString(), result.UserId);
+        Assert.Equal(user.Id.ToString(), result.UserId);
         Assert.NotNull(result);
         Assert.IsType<Cart>(result);
     }
@@ -47,9 +48,6 @@ public class CartServiceTests
     public async void AddProductToCart_ValidCart_Test()
     {
         // arrange 
-        var user = new User { Id = "1" };
-        var cart = new Cart { Id = Guid.NewGuid(), UserId = user.Id, TotalPrice = 0 };
-        var product = new Product { Id = Guid.NewGuid(), Price = 39m, Name = "Test", UserId = user.Id };
         _mockRepoManager.Setup(mang => mang.ProductsRepository.GetProductByIdAsync(product.Id)).Returns(Task.FromResult(product));
 
         _mockRepoManager.Setup(mang => mang.CartRepository.GetCartByUserIdAsync(user.Id)).Returns(Task.FromResult(cart));
@@ -58,9 +56,24 @@ public class CartServiceTests
         await _cartService.AddProductToCartAsync(user.Id, product.Id.ToString());
 
         // assert 
-        Assert.Equal(cart.TotalPrice, product.Price);
+        Assert.Equal(cart.TotalPrice, product.Price + product.Price);
         Assert.NotEmpty(cart.UserId);
-        Assert.Equal(1, cart.CartItems.FirstOrDefault().Quantity);
         _mockRepoManager.Verify(service => service.SaveAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async void RemoveProductFromCart_()
+    {
+        // arrange
+        _mockRepoManager.Setup(x => x.ProductsRepository.GetProductByIdAsync(product.Id)).Returns(Task.FromResult(product));
+        _mockRepoManager.Setup(x => x.CartRepository.GetCartByIdAsync(cart.Id)).Returns(Task.FromResult(cart));
+
+        // act 
+        await _cartService.RemoveProductFromCartAsync(cart.Id, product.Id);
+
+        // assert 
+        Assert.Equal(cart.TotalPrice, 0);
+        Assert.Empty(cart.CartItems);
+        _mockRepoManager.Verify(x => x.SaveAsync(), Times.Once);
     }
 }
